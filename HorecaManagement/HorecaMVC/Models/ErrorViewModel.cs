eacb064953b9c02db5bf91d54e20b99079d7ebf3
Jonas De Corte <Jonas.DeCorte@realdolmen.com>
2022-02-28 15:11:04 +0100

    fresh project

---
+++ HorecaManagement/HorecaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HorecaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
