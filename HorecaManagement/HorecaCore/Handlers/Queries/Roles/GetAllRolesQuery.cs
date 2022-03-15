64860fd3b9851d520fbe3c38ad2120d881218cce
Jonas De Corte <Jonas.DeCorte@realdolmen.com>
2022-03-15 16:17:43 +0100

    added logs for roles

--- HorecaManagement/HorecaCore/Handlers/Queries/Roles/GetAllRolesQuery.cs
+++ HorecaManagement/HorecaCore/Handlers/Queries/Roles/GetAllRolesQuery.cs
@@ -1,6 +1,7 @@
 ﻿using Horeca.Shared.Dtos.Accounts;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using NLog;
 
 namespace Horeca.Core.Handlers.Queries.Roles
 
@@ -15,6 +16,7 @@ namespace Horeca.Core.Handlers.Queries.Roles
     public class GetAllRolesQueryHandler : IRequestHandler<GetAllRolesQuery, IEnumerable<RoleDto>>
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private static Logger logger = LogManager.GetCurrentClassLogger();
 
         public GetAllRolesQueryHandler(RoleManager<IdentityRole> roleManager)
 
@@ -24,11 +26,15 @@ namespace Horeca.Core.Handlers.Queries.Roles
 
         public async Task<IEnumerable<RoleDto>> Handle(GetAllRolesQuery request, CancellationToken cancellationToken)
         {
-            return await Task.FromResult(roleManager.Roles.Select(x => new RoleDto
+            var result = await Task.FromResult(roleManager.Roles.Select(x => new RoleDto
             {
                 Id = x.Id,
                 RoleName = x.Name
             }).ToList());
+
+            logger.Info("{amount} of {nameof} have been returned", result.Count(), nameof(RoleDto));
+
+            return result;
         }
     }
 }
