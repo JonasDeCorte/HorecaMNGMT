3b43fa3d1b91dfc651fe23c9d8cf5a007e52a443
Jonas De Corte <Jonas.DeCorte@realdolmen.com>
2022-04-12 15:46:00 +0200

    Interface merge

--- HorecaManagement/HorecaShared/Data/Repositories/IRestaurantRepository.cs
+++ HorecaManagement/HorecaShared/Data/Repositories/IRestaurantRepository.cs
@@ -1,12 +1,11 @@
 ﻿using Horeca.Shared.Data.Entities;
-using static Horeca.Shared.Utils.Constants;
 
 namespace Horeca.Shared.Data.Repositories
 {
     public interface IRestaurantRepository : IRepository<Restaurant>
     {
-        Task<Restaurant> GetRestaurantByIdWithOrdersWithOrderState(int restaurantId, OrderState orderState);
-
         Task<Restaurant> GetRestaurantIncludingDependenciesById(int restaurantId);
+
+        Task<Restaurant> GetRestaurantIncludingMenuCardsById(int restaurantId);
     }
 }
