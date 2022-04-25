b442ab72d3a6eb8e465f9b0006965975a6d659fe
Jonas De Corte <Jonas.DeCorte@realdolmen.com>
2022-04-25 12:25:21 +0200

    fixed ingredient add to dish

---
+++ HorecaManagement/HorecaCore/Exceptions/EntityIsAlreadyPartOfThisCollectionException.cs
@@ -0,0 +1,11 @@
+﻿namespace Horeca.Core.Exceptions
+{
+    public class EntityIsAlreadyPartOfThisCollectionException : Exception
+    {
+        public EntityIsAlreadyPartOfThisCollectionException() : base("entity is already in this collection")
+        {
+        }
+
+        public static EntityIsAlreadyPartOfThisCollectionException Instance { get; } = new();
+    }
+}
