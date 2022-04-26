33f4afbfcefeb500d7227f80fb9b16fd9efebb23
Robbe Kerkaert <robbe.kerkaert@student.hogent.be>
2022-04-26 09:35:52 +0200

    ClassConstants merge fix

--- HorecaManagement/HorecaShared/Constants/ClassConstants.cs
+++ HorecaManagement/HorecaShared/Constants/ClassConstants.cs
@@ -2,50 +2,76 @@
 {
     public static class ClassConstants
     {
+        public const string Id = "Id";
         public const string Unit = "Unit";
 
         public const string Ingredient = "Ingredient";
+        public const string IngredientId = "ingredientId";
+
         public const string Ingredients = "ingredients";
 
         public const string Dish = "Dish";
+        public const string DishId = "dishId";
+
         public const string Dishes = "dishes";
 
         public const string Menu = "Menu";
+        public const string MenuId = "menuId";
+
         public const string Menus = "menus";
 
         public const string MenuCard = "MenuCard";
         public const string MenuCards = "menuCards";
 
         public const string Restaurant = "Restaurant";
+        public const string RestaurantId = "restaurantId";
+
         public const string Employee = "Employee";
+        public const string EmployeeId = "employeeId";
+
 
         public const string Schedule = "Schedule";
+        public const string ScheduleId = "scheduleId";
+
         public const string All = "All";
 
         public const string Booking = "Booking";
         public const string BookingStatus = "BookingStatus";
+        public const string Status = "status";
+
         public const string BookingNo = "BookingNo";
         public const string Details = "Details";
         public const string Member = "Member";
         public const string ListCount = "ListCount";
 
         public const string Order = "Order";
+        public const string OrderId = "orderId";
+        public const string OrderState = "orderState";
+
         public const string Orders = "Orders";
         public const string OrderLine = "OrderLine";
+        public const string OrderLineId = "orderLineId";
+
         public const string Table = "Table";
+        public const string TableId = "tableId";
+
         public const string Prepare = "Prepare";
         public const string Ready = "Ready";
 
         public const string Account = "Account";
         public const string User = "User";
         public const string Users = "Users";
-        public const string Username = "username";
+        public const string UserId = "userId";
+
         public const string Admin = "Admin";
         public const string Login = "login";
         public const string Register = "register";
         public const string RegisterAdmin = "register-admin";
         public const string RefreshToken = "RefreshToken";
+        public const string Revoke = "revoke";
+        public const string GetUserClaims = "GetUserClaims";
 
+        public const string SuperAdminAccessToken = "SuperAdminAccessToken";
         public const string UserPermissions = "UserPermissions";
         public const string Permission = "Permission";
     }
