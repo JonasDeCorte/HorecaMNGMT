61827d0201697a98f5f5a655cc42f8049f7fe13c
Jonas De Corte <Jonas.DeCorte@realdolmen.com>
2022-04-26 10:53:30 +0200

    fix

--- HorecaManagement/HorecaShared/Constants/RouteConstants.cs
+++ HorecaManagement/HorecaShared/Constants/RouteConstants.cs
@@ -93,10 +93,10 @@
             public const string Post = $"{ClassConstants.Restaurant}";
 
             public const string Get = $"{ClassConstants.Table}{Slash}{ClassConstants.Details}";
-            public const string GetOrderState = $"{ClassConstants.Restaurant}{Slash}{ClassConstants.Order}{Slash}{ClassConstants.OrderState}";
-            public const string DeliverOrder = $"{ClassConstants.Restaurant}{Slash}{ClassConstants.Order}";
-            public const string PrepareOrderLine = $"{ClassConstants.Restaurant}{Slash}{ClassConstants.Order}{Slash}{ClassConstants.OrderLine}{Slash}{ClassConstants.Prepare}";
-            public const string ReadyOrderLine = $"{ClassConstants.Restaurant}{Slash}{ClassConstants.Order}{Slash}{ClassConstants.OrderLine}{Slash}{ClassConstants.Ready}";
+            public const string GetOrderState = $"{ClassConstants.Restaurant}{Slash}{ClassConstants.OrderState}";
+            public const string DeliverOrder = $"{ClassConstants.Restaurant}";
+            public const string PrepareOrderLine = $"{ClassConstants.Restaurant}{Slash}{ClassConstants.OrderLine}{Slash}{ClassConstants.Prepare}";
+            public const string ReadyOrderLine = $"{ClassConstants.Restaurant}{Slash}{ClassConstants.OrderLine}{Slash}{ClassConstants.Ready}";
         }
 
         public static class PermissionConstants
