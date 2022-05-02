540aec8ddd8304326e7a66a1d3abc428b94a511b
Robbe Kerkaert <Robbe.Kerkaert@realdolmen.com>
2022-05-02 10:32:39 +0200

    removepermissions fix

---
+++ HorecaManagement/HorecaMVC/Controllers/FloorplanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HorecaMVC.Controllers
+{
+    public class FloorplanController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HorecaManagement/HorecaMVC/Controllers/TableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HorecaMVC.Controllers
+{
+    public class TableController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
