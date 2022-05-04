2687bd264b1373482539abf4786a37e6d9cc735d
Robbe Kerkaert <Robbe.Kerkaert@realdolmen.com>
2022-05-04 16:16:25 +0200

    added edit table

--- HorecaManagement/HorecaAPI/Controllers/TableController.cs
+++ HorecaManagement/HorecaAPI/Controllers/TableController.cs
@@ -60,7 +60,7 @@ namespace Horeca.API.Controllers
         /// <summary>
         /// Adds tables for all the bookings with the specified restaurant schedule Id
         /// </summary>
-        /// <param name="ScheduleId">ScheduleId</param>
+        /// <param name="scheduleId">ScheduleId</param>
         /// <param name="model">mutateTableDto object</param>
         /// <returns>
         ///list of added tables for each booking for the specified restaurant schedule id
@@ -72,16 +72,16 @@ namespace Horeca.API.Controllers
         [PermissionAuthorize(nameof(Table), Permissions.Create)]
         [ProducesResponseType(typeof(TableDto), (int)HttpStatusCode.Created)]
         [ProducesErrorResponseType(typeof(BaseResponseDto))]
-        public async Task<IActionResult> Post(int ScheduleId, [FromBody] MutateTableDto model)
+        public async Task<IActionResult> Post(int scheduleId, [FromBody] MutateTableDto model)
         {
-            return StatusCode((int)HttpStatusCode.Created, await mediator.Send(new AddTableForRestaurantScheduleCommand(model, ScheduleId)));
+            return StatusCode((int)HttpStatusCode.Created, await mediator.Send(new AddTableForRestaurantScheduleCommand(model, scheduleId)));
         }
 
         /// <summary>
         /// Adds tables for all tables within the specified floor plan object
         /// </summary>
-        /// <param name="FloorplanId">FloorplanId</param>
-        /// <param name="RestaurantId">RestaurantId</param>
+        /// <param name="floorplanId">FloorplanId</param>
+        /// <param name="restaurantId">RestaurantId</param>
         /// <param name="model">FloorplanDetailDto object</param>
         /// <returns>
         /// list of tableIds from created tables
@@ -93,9 +93,43 @@ namespace Horeca.API.Controllers
         [PermissionAuthorize(nameof(Table), Permissions.Create)]
         [ProducesResponseType(typeof(TableDto), (int)HttpStatusCode.Created)]
         [ProducesErrorResponseType(typeof(BaseResponseDto))]
-        public async Task<IActionResult> AddTablesFromFloorplan(int FloorplanId, int RestaurantId, [FromBody] FloorplanDetailDto model)
+        public async Task<IActionResult> AddTablesFromFloorplan(int floorplanId, int restaurantId, [FromBody] FloorplanDetailDto model)
         {
-            return StatusCode((int)HttpStatusCode.Created, await mediator.Send(new CreateTablesFromFloorplanCommand(model, FloorplanId, RestaurantId)));
+            return StatusCode((int)HttpStatusCode.Created, await mediator.Send(new CreateTablesFromFloorplanCommand(model, floorplanId, restaurantId)));
+        }
+
+        /// <summary>
+        ///  Delete an existing Table
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="204">Success delete an existing Table</response>
+        /// <response code="400">Bad request</response
+        [PermissionAuthorize(nameof(Table), Permissions.Delete)]
+        [HttpDelete]
+        [Route(RouteConstants.TableConstants.Delete)]
+        [ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(BaseResponseDto))]
+        public async Task<IActionResult> DeleteById(int id)
+        {
+            return StatusCode((int)HttpStatusCode.OK, await mediator.Send(new DeleteTableCommand(id)));
+        }
+
+        /// <summary>
+        /// Update an existing Table
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        /// <response code="200">Success updating existing Table</response>
+        /// <response code="400">Bad request</response>
+        [PermissionAuthorize(nameof(Table), Permissions.Update)]
+        [HttpPut]
+        [Route(RouteConstants.TableConstants.Update)]
+        [ProducesResponseType(typeof(int), (int)HttpStatusCode.OK)]
+        [ProducesErrorResponseType(typeof(BaseResponseDto))]
+        public async Task<IActionResult> Update([FromBody] MutateTableDto model, int id, int floorplanId)
+        {
+            return StatusCode((int)HttpStatusCode.OK, await mediator.Send(new EditTableCommand(model, id, floorplanId)));
         }
     }
 }
--- HorecaManagement/HorecaCore/Handlers/Commands/Tables/EditTableCommandHandler.cs
+++ HorecaManagement/HorecaCore/Handlers/Commands/Tables/EditTableCommandHandler.cs
@@ -44,9 +44,60 @@ namespace Horeca.Core.Handlers.Commands.Tables
                 throw new EntityNotFoundException();
             }
 
-            table.Name = request.Model.Name ?? table.Name;
-            table.Pax = request.Model.Pax ?? table.Pax;
             table.Seats = request.Model.Seats ?? table.Seats;
+            table.Name = request.Model.Name ?? table.Name;
+            table.Src = request.Model.Src ?? table.Src;
+            table.Type = request.Model.Type ?? table.Type;
+            table.Version = request.Model.Version ?? table.Version;
+            table.OriginX = request.Model.OriginX ?? table.OriginX;
+            table.OriginY = request.Model.OriginY ?? table.OriginY;
+            table.Fill = request.Model.Fill ?? table.Fill;
+            table.StrokeLineCap = request.Model.StrokeLineCap ?? table.StrokeLineCap;
+            table.StrokeLineJoin = request.Model.StrokeLineJoin ?? table.StrokeLineJoin;
+            table.BackgroundColor = request.Model.BackgroundColor ?? table.BackgroundColor;
+            table.FillRule = request.Model.FillRule ?? table.FillRule;
+            table.PaintFirst = request.Model.PaintFirst ?? table.PaintFirst;
+            table.GlobalCompositeOperation = request.Model.GlobalCompositeOperation ?? table.GlobalCompositeOperation;
+            if (request.Model.Pax != table.Pax)
+                table.Pax = request.Model.Pax;
+            if (request.Model.Left != table.Left)
+                table.Left = request.Model.Left;
+            if (request.Model.Top != table.Top)
+                table.Top = request.Model.Top;
+            if (request.Model.Width != table.Width)
+                table.Width = request.Model.Width;
+            if (request.Model.Height != table.Height)
+                table.Height = request.Model.Height;
+            if (request.Model.StrokeWidth != table.StrokeWidth)
+                table.StrokeWidth = request.Model.StrokeWidth;
+            if (request.Model.StrokeDashOffset != table.StrokeDashOffset)
+                table.StrokeDashOffset = request.Model.StrokeDashOffset;
+            if (request.Model.StrokeMiterLimit != table.StrokeMiterLimit)
+                table.StrokeMiterLimit = request.Model.StrokeMiterLimit;
+            if (request.Model.StrokeUniform != table.StrokeUniform)
+                table.StrokeUniform = request.Model.StrokeUniform;
+            if (request.Model.ScaleX != table.ScaleX)
+                table.ScaleX = request.Model.ScaleX;
+            if (request.Model.ScaleY != table.ScaleY)
+                table.ScaleY = request.Model.ScaleY;
+            if (request.Model.Angle != table.Angle)
+                table.Angle = request.Model.Angle;
+            if (request.Model.FlipX != table.FlipX)
+                table.FlipX = request.Model.FlipX;
+            if (request.Model.FlipY != table.FlipY)
+                table.FlipY = request.Model.FlipY;
+            if (request.Model.Opacity != table.Opacity)
+                table.Opacity = request.Model.Opacity;
+            if (request.Model.Visible != table.Visible)
+                table.Visible = request.Model.Visible;
+            if (request.Model.SkewX != table.SkewX)
+                table.SkewX = request.Model.SkewX;
+            if (request.Model.SkewY != table.SkewY)
+                table.SkewY = request.Model.SkewY;
+            if (request.Model.CropX != table.CropX)
+                table.CropX = request.Model.CropX;
+            if (request.Model.CropY != table.CropY)
+                table.CropY = request.Model.CropY;
 
             repository.Tables.Update(table);
 
