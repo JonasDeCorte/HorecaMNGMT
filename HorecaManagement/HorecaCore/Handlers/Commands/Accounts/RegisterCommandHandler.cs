b4887e7e1a2d8a1e3146528d4bb803f700580d78
Jonas De Corte <Jonas.DeCorte@realdolmen.com>
2022-05-17 16:38:01 +0200

    nieuwe user moet basispermissies krijgen (restaurant read, user read)

--- HorecaManagement/HorecaCore/Handlers/Commands/Accounts/RegisterCommandHandler.cs
+++ HorecaManagement/HorecaCore/Handlers/Commands/Accounts/RegisterCommandHandler.cs
@@ -57,13 +57,11 @@ namespace Horeca.Core.Handlers.Commands.Accounts
             {
                 logger.Info("added new user {user}", user.NormalizedUserName);
 
-                UserPermission userPerm = AddNewUserPermission(user);
-
+                AddNewUserPermission(user);
+                AddDefaultUserPermissions(user);
                 IsOwnerPermissions(user);
 
                 await repository.CommitAsync();
-
-                logger.Info("added default permission new user {userperm}", userPerm);
             }
 
             if (!result.Succeeded)
@@ -75,18 +73,51 @@ namespace Horeca.Core.Handlers.Commands.Accounts
             return user.Id;
         }
 
-        private UserPermission AddNewUserPermission(ApplicationUser user)
+        private void AddDefaultUserPermissions(ApplicationUser user)
         {
-            var NewUserPerm = repository.PermissionRepository.Get(1);
+            var allPerms = repository.PermissionRepository.GetAll();
+            List<Permission> permsToAdd = new();
+
+            var bookings = allPerms.Where(x => x.Name.StartsWith("Booking_"));
+            var restaurantRead = allPerms.Where(x => x.Name.StartsWith("Restaurant_Read"));
+            var scheduleRead = allPerms.Where(x => x.Name.StartsWith("Schedule_Read"));
+            var menuCardRead = allPerms.Where(x => x.Name.StartsWith("MenuCard_Read"));
+            var menuRead = allPerms.Where(x => x.Name.StartsWith("Menu_Read"));
+            var dishRead = allPerms.Where(x => x.Name.StartsWith("Dish_Read"));
+            var applicationUserRead = allPerms.Where(x => x.Name.StartsWith("ApplicationUser_Read"));
+
+
+            permsToAdd.AddRange(bookings);
+            permsToAdd.AddRange(restaurantRead);
+            permsToAdd.AddRange(menuCardRead);
+            permsToAdd.AddRange(menuRead);
+            permsToAdd.AddRange(dishRead);
+            permsToAdd.AddRange(applicationUserRead);
+
+            foreach (var item in permsToAdd)
+            {
+                var userPerm = new UserPermission
+                {
+                    PermissionId = item.Id,
+                    UserId = user.Id
+                };
+
+                repository.UserPermissions.Add(userPerm);
+            }
+        }
+
+        private void AddNewUserPermission(ApplicationUser user)
+        {
+            var newUserPerm = repository.PermissionRepository.Get(1);
 
             var userPerm = new UserPermission
             {
-                PermissionId = NewUserPerm.Id,
+                PermissionId = newUserPerm.Id,
                 UserId = user.Id
             };
+            logger.Info("added default permission new user {userperm}", userPerm);
 
             repository.UserPermissions.Add(userPerm);
-            return userPerm;
         }
 
         private void IsOwnerPermissions(ApplicationUser user)
