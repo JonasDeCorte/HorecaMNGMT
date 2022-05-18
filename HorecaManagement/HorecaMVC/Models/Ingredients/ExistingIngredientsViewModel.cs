1a9935a07c10d9cda580b95828e92af08c4c6a64
Jonas De Corte <Jonas.DeCorte@realdolmen.com>
2022-05-18 10:31:06 +0200

    fix selectlists

--- HorecaManagement/HorecaMVC/Models/Ingredients/ExistingIngredientsViewModel.cs
+++ HorecaManagement/HorecaMVC/Models/Ingredients/ExistingIngredientsViewModel.cs
@@ -4,12 +4,9 @@ namespace Horeca.MVC.Models.Ingredients
 {
     public class ExistingIngredientsViewModel : IngredientListViewModel
     {
-        [Required(ErrorMessage = "RestaurantId is required.")]
-        [Range(1, int.MaxValue, ErrorMessage = "RestaurantId Id can't be 0")]
+       
         public int RestaurantId { get; set; }
 
-        [Required(ErrorMessage = "DishId is required.")]
-        [Range(1, int.MaxValue, ErrorMessage = "Dish Id can't be 0")]
         public int DishId { get; set; }
 
         [Required(ErrorMessage = "IngredientId is required.")]
--- HorecaManagement/HorecaMVC/Models/Menus/ExistingMenuDishesViewModel.cs
+++ HorecaManagement/HorecaMVC/Models/Menus/ExistingMenuDishesViewModel.cs
@@ -5,12 +5,9 @@ namespace Horeca.MVC.Models.Menus
 {
     public class ExistingMenuDishesViewModel : DishListViewModel
     {
-        [Required(ErrorMessage = "RestaurantId is required.")]
-        [Range(1, int.MaxValue, ErrorMessage = "RestaurantId Id can't be 0")]
         public int RestaurantId { get; set; }
 
-        [Required(ErrorMessage = "MenuId is required.")]
-        [Range(1, int.MaxValue, ErrorMessage = "MenuId Id can't be 0")]
+       
         public int MenuId { get; set; }
 
         [Required(ErrorMessage = "DishId is required.")]
