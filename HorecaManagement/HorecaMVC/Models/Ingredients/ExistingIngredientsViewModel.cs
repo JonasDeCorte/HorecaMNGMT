6b3f8301351c73b9ec3b81ad01fb906dd8d1fade
Jonas De Corte <Jonas.DeCorte@realdolmen.com>
2022-05-18 09:19:34 +0200

    add existing ingredient if list empty kan ik nog steeds op add klikken

--- HorecaManagement/HorecaMVC/Models/Ingredients/ExistingIngredientsViewModel.cs
+++ HorecaManagement/HorecaMVC/Models/Ingredients/ExistingIngredientsViewModel.cs
@@ -1,11 +1,19 @@
-﻿namespace Horeca.MVC.Models.Ingredients
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Horeca.MVC.Models.Ingredients
 {
     public class ExistingIngredientsViewModel : IngredientListViewModel
     {
+        [Required(ErrorMessage = "RestaurantId is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "RestaurantId Id can't be 0")]
         public int RestaurantId { get; set; }
 
+        [Required(ErrorMessage = "DishId is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Dish Id can't be 0")]
         public int DishId { get; set; }
 
+        [Required(ErrorMessage = "IngredientId is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "IngredientId Id can't be 0")]
         public int IngredientId { get; set; }
     }
 }
