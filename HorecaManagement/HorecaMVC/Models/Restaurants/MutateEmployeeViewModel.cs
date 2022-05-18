79602e945f0deeab024cc3c5ff79d489faae1af1
Jonas De Corte <Jonas.DeCorte@realdolmen.com>
2022-05-18 09:44:11 +0200

    revert  add employee

--- HorecaManagement/HorecaMVC/Models/Restaurants/MutateEmployeeViewModel.cs
+++ HorecaManagement/HorecaMVC/Models/Restaurants/MutateEmployeeViewModel.cs
@@ -1,16 +1,11 @@
 ﻿using Horeca.MVC.Models.Accounts;
-using System.ComponentModel.DataAnnotations;
 
 namespace Horeca.MVC.Models.Restaurants
 {
     public class MutateEmployeeViewModel
     {
-        [Required(ErrorMessage = "RestaurantId is required.")]
-        [Range(1, int.MaxValue, ErrorMessage = "RestaurantId Id can't be 0")]
         public int RestaurantId { get; set; }
 
-        [Required(ErrorMessage = "EmployeeId is required.")]
-        [Range(1, int.MaxValue, ErrorMessage = "EmployeeId Id can't be 0")]
         public string EmployeeId { get; set; }
 
         public List<UserViewModel> Employees { get; set; } = new List<UserViewModel>();
