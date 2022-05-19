4dcae0800aff57e8f82523ce6ac06b99151271ae
Robbe Kerkaert <Robbe.Kerkaert@realdolmen.com>
2022-05-19 10:46:37 +0200

    fixed pax AND seats being editable in table edit

--- HorecaManagement/HorecaMVC/Models/Tables/TableViewModel.cs
+++ HorecaManagement/HorecaMVC/Models/Tables/TableViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace Horeca.MVC.Models.Tables
+﻿using Horeca.Shared.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Horeca.MVC.Models.Tables
 {
     public class TableViewModel
     {
@@ -10,10 +13,11 @@
 
         public int BookingDetailId { get; set; }
 
-        public int? Pax { get; set; }
-
+        [Required]
         public string? Seats { get; set; }
 
+        [Required]
+        [StringLength(50, ErrorMessage = ErrorConstants.StringLength50)]
         public string Name { get; set; }
 
         public string Src { get; set; }
